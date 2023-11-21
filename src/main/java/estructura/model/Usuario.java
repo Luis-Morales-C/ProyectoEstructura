9a72d8e019d070c707f92a90a1e291c95b092b1e
cos-03 <112827893+cos-03@users.noreply.github.com>
2023-11-21 15:26:09 -0500

    interfaz lista

---
+++ src/main/java/estructura/model/Usuario.java
@@ -0,0 +1,32 @@
+package estructura.model;
+
+import java.io.Serializable;
+
+public class Usuario implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String nombreUsuario;
+    private String contrasena;
+
+    public Usuario(String nombreUsuario, String contrasena) {
+        this.nombreUsuario = nombreUsuario;
+        this.contrasena = contrasena;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+}
+
